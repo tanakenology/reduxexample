5705d0dc6c03ed86d73530f19a0dd652c6b620f9
tanakenology <x27c5343bd78n@gmail.com>
2019-06-01 12:19:17 +0900

    Finish Redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
+import store from './store'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
